c32e582a4de7f9ea0c0d065b1d9fe8d78f554235
Gleb Nerzhin <grach.scheglov@gmail.com>
2017-05-14 16:07:00 +0300

    first cosmetic changes

--- 02rockpaperscissors/app.js
+++ 02rockpaperscissors/app.js
@@ -1,3 +1,22 @@
+let images = [
+	0, 
+	'icons/rock.png',
+	'icons/paper.png',
+	'icons/scissors.png',
+	'icons/lizard.png',
+	'icons/spock.png'
+];
+let battle = {
+	enemyMove: '',
+	yourMove: ''
+};
+let counters = {
+	won: 11,
+	lost: 2,
+	kda: function() {
+		return Number(this.won/this.lost);
+	}
+};
 function getEnemyAvatar() {
 	let random = Math.floor(Math.random()*100 + 1);
 	const enemyAvatar = document.querySelector('.enemy-photo');
@@ -10,5 +29,44 @@ function healthBar() {
 	healthBar.textContent = '45%';
 };
 
-getEnemyAvatar();
+function enemyMove() {
+	let random = Math.floor(Math.random() * 5) + 1;
+	let elem = document.createElement('img');
+	elem.src = images[random];
+	elem.className = 'enemy-pick';
+	document.querySelector('.enemy-move').appendChild(elem);
+	battle.enemyMove = random;
+};
 
+function eraseBattleDeck() {
+	document.querySelector('.enemy-move').removeChild(document.querySelector('.enemy-pick'));
+};
+
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const lizardBtn = document.querySelector('#lizard');
+const spockBtn = document.querySelector('#spock');
+
+rockBtn.addEventListener('click', yourMove(1));
+paperBtn.addEventListener('click', yourMove(2));
+scissorsBtn.addEventListener('click', yourMove(3));
+lizardBtn.addEventListener('click', yourMove(4));
+spockBtn.addEventListener('click', yourMove(5));
+
+function yourMove(value) {
+	battle.yourMove = value;
+};
+
+function game() {
+
+}
+
+const counterWon = document.querySelector('.won').querySelector('span');
+const counterLost = document.querySelector('.lost').querySelector('span');
+const counterKDA = document.querySelector('.kda').querySelector('span');
+
+counterWon.textContent = counters.won;
+counterLost.textContent = counters.lost;
+counterKDA.textContent = counters.kda();
+getEnemyAvatar();

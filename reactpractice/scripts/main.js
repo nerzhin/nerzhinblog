5c10cbbac1cc7f5d3488ff76f0ace2d90ac5f1c4
Gleb Nerzhin <grach.scheglov@gmail.com>
2017-05-31 23:20:14 +0300

    'lesson10'

--- reactpractice/scripts/main.js
+++ reactpractice/scripts/main.js
@@ -15,7 +15,18 @@ let elem = document.querySelector('main');
 /* Main app */
 
 let App = React.createClass({
+		getInitialState: function() {
+			return {
+				fishes: {},
+				order: {}
+			}
+		},
+		addFish: function(fish) {
+			let timeStamp = (new Date()).getTime();
 
+			this.state.fishes["fish-" + timeStamp] = fish;
+			this.setState({ fishes: this.state.fishes });
+		},
 		render: function() {
 			return (
 				<div className="catch-of-the-day">
@@ -66,16 +77,56 @@ let Inventory = React.createClass({
 
 		render: function() {
 			return (
-				<p>Inventory</p>
+				<div>
+					<h2>Inventory</h2>
+					<AddFishForm {...this.props}/>
+				</div>
 				)
 		}
+});
+
+/* Add fish form 
+<AddFishForm/>
+*/
+
+let AddFishForm = React.createClass({
+	createFish: function(event) {
+		event.preventDefault();
+		let fish = {
+			name: this.refs.name.value,
+			price: this.refs.price.value,
+			status: this.refs.status.value,
+			desc: this.refs.desc.value,
+			image: this.refs.image.value,
+		};
+
+		this.props.addFish(fish);
+		this.refs.fishForm.reset();
+	},
+
+	render: function() {
+		return (
+			
+			<form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+				<input type="text" ref="name" placeholder="Fish Name"/>
+				<input type="text" ref="price" placeholder="Fish Price"/>
+				<select ref="status">
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea type="text" ref="desc" placeholder="Description"></textarea>
+				<input type="text" ref="image" placeholder="URL to image"/>
+				<button type="submit">+ Add item</button>
+			</form>
+		)
+	}
 });
 
 /* <StorePicker/> */
 let StorePicker = React.createClass({
 	mixins : [History],
 	goToStore: function(event) {
-		let storeId = this.refs.storeId.value;
+		let storeId = littleHelp.clearingText(this.refs.storeId.value);
 		event.preventDefault();
 		
 		this.history.pushState(null, '/store/'+storeId);
